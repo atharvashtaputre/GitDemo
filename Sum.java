50c874262de671d53358687675868945a02642d6
Atharv <atharvashtaputre5@gmail.com>
2023-02-01 18:59:33 +0530

    Commit inside dev

--- Sum.java
+++ Sum.java
@@ -2,7 +2,7 @@ public class Sum{
       public static void main (String args[]) {
             int a = 5;
             int b = 10;
-            System.out.println("The sum is : "+(a+b));
+            System.out.println("The sum is as mentioned : "+(a+b));
       }
 
 }
